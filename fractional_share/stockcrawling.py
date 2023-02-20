10c0c135d3932ffd5b459f9ca6e6e0cacb1ec334
yewonbahn <banyaewon98@gmail.com>
2023-02-21 00:19:36 +0900

    feat: 상세페이지 크롤링 코드 작성

---
+++ fractional_share/stockcrawling.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://finance.naver.com/item/main.naver?code=900110'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    company_info = soup.select_one('#summary_info')
+    #당기순이익(억원)
+    profit1 = soup.select_one('#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(3) > td:nth-child(2)')
+    profit2 = soup.select_one('#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(3) > td:nth-child(3) > em')
+    profit3 = soup.select_one('#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(3) > td:nth-child(4)')
+    #매출액(억원)
+    sales1  = soup.select_one('#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(1) > td:nth-child(2)')
+    sales2  = soup.select_one('#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(1) > td:nth-child(3)')
+    sales3  = soup.select_one('#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(1) > td:nth-child(4)')
+    #뉴스
+    news  = soup.select('#content > div.section.new_bbs > div.sub_section.news_section > ul > li')
+    for news in news:
+        print(news.get_text().replace("\n",":"))
+
+    #차트
+    week  = 'https://ssl.pstatic.net/imgfinance/chart/item/area/week/900110.png?sidcode=1676905742196'
+    month3  = 'https://ssl.pstatic.net/imgfinance/chart/item/area/month3/900110.png?sidcode=1676905742196'
+    year  = 'https://ssl.pstatic.net/imgfinance/chart/item/area/year/900110.png?sidcode=1676905742196'
+    year3  = 'https://ssl.pstatic.net/imgfinance/chart/item/area/year3/900110.png?sidcode=1676905742196'
+
+
+    print(company_info.get_text())
+    print(int(profit1.get_text()))
+    print(int(profit2.get_text()))
+    print(int(profit3.get_text()))
+
+    print(int(sales1.get_text()))
+    print(int(sales2.get_text()))
+    print(int(sales3.get_text()))
+
+else : 
+    print(response.status_code)
