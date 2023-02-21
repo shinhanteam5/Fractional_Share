d082f269d58390e802e97c73a3065d8eb34db484
yewonbahn <banyaewon98@gmail.com>
2023-02-21 16:41:15 +0900

    feat: news model , url, view 추가

--- fractional_share/stockcrawling.py
+++ fractional_share/stockcrawling.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://finance.naver.com/item/main.nhn?code=112040'
+url = 'https://finance.naver.com/item/main.nhn?code=000020'
 
 response = requests.get(url)
 
@@ -9,11 +9,11 @@ if response.status_code == 200:
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
     info = soup.select_one('#summary_info')
-
+    print(info)
     current_price = soup.select_one('#middle > dl > dd:nth-child(6)')
-    #당기순이익(억원)
+    #당기순이익(억원)s
     profit1 = soup.select_one('#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(3) > td:nth-child(2)')
-    profit2 = soup.select_one('#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(3) > td:nth-child(3) > em')
+    profit2 = soup.select_one('#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(3) > td:nth-child(3)')
     profit3 = soup.select_one('#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(3) > td:nth-child(4)')
     #매출액(억원)
     sales1  = soup.select_one('#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(1) > td:nth-child(2)')
@@ -34,7 +34,7 @@ if response.status_code == 200:
     # print(info.get_text())
     # print(int(profit1.get_text()))
     # print(int(profit2.get_text()))
-    print(profit3.get_text().strip())
+    print(profit2.get_text)
 
     # print(int(sales1.get_text()))
     # print(int(sales2.get_text()))
