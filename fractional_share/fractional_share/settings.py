5d8e8e0434c4ff5d60db2a515917bca51e1b9893
yewonbahn <banyaewon98@gmail.com>
2023-02-23 07:57:55 +0900

    feat: error fix

--- fractional_share/fractional_share/settings.py
+++ fractional_share/fractional_share/settings.py
@@ -140,11 +140,11 @@ SILENCED_SYSTEM_CHECKS=['urls.W002']
 if DEBUG:
     CORS_ALLOW_ALL_ORIGINS=True
 
-REST_FRAMEWORK = {
-'DEFAULT_AUTHENTICATION_CLASSES': [
-    'rest_framework.authentication.TokenAuthentication',
-],
-'DEFAULT_PERMISSION_CLASSES': [
-    'rest_framework.permissions.IsAuthenticated',
-],
-}
+# REST_FRAMEWORK = {
+# 'DEFAULT_AUTHENTICATION_CLASSES': [
+#     'rest_framework.authentication.TokenAuthentication',
+# ],
+# 'DEFAULT_PERMISSION_CLASSES': [
+#     'rest_framework.permissions.IsAuthenticated',
+# ],
+# }
