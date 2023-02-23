110934635121df65ad9f8053641c8d65941aeba5
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-02-23 10:23:40 +0900

    feat: db 수정

--- fractional_share/fractional_share/urls.py
+++ fractional_share/fractional_share/urls.py
@@ -17,21 +17,21 @@ from django.contrib import admin
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 from . import views
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="API Documentation",
-        default_version='v1',
-        description="API documentation",
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="API Documentation",
+#         default_version='v1',
+#         description="API documentation",
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
 urlpatterns = [
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/member', include('member.urls')),
     path('api/portfolio', include('portfolio.urls')),
